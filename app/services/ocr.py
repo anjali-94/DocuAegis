a4f0b301055cd13fe72182402da3dfe769b4e54b
anjali-94 <anjalishr987@gmail.com>
2025-05-27 13:44:23 +0530

    second commit

--- app/services/ocr.py
+++ app/services/ocr.py
@@ -1,9 +1,1 @@
-# app/services/ocr.py
-import pytesseract
-from PIL import Image
-import io
 
-def process(file):
-    image = Image.open(io.BytesIO(file.file.read()))
-    text = pytesseract.image_to_string(image)
-    return {"extracted_text": text}
